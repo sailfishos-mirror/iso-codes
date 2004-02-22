3330c3350522315d1ee77a616a1b771746363408
Alastair McKinstry <mckinstry@debian.org>
2004-02-22 18:58:06 +0000

    Initial revision

---
+++ Makefile
@@ -0,0 +1,31 @@
+#!/usr/bin/make -f
+#
+# Top-level Makefile for iso-codes
+
+# These may be overridden by values from debian/rules, etc.
+PREFIX=/usr
+PREFIX_QUOTED=\/usr
+VERSION=0.21
+
+DOMAINS=iso_3166 iso_639 iso_4217 iso_3166_1 iso_3166_2 iso_3166_3
+
+SUBDIRS=iso_3166 iso_639 iso_4217
+
+all: iso-codes.pc
+	for d in ${SUBDIRS} ; do ${MAKE} -C $$d all; done
+
+clean:
+	for d in ${SUBDIRS} ; do ${MAKE} -C $$d clean ; done
+	rm -f iso-codes.pc
+
+install: iso-codes.pc
+	for d in ${SUBDIRS} ; do ${MAKE} -C $$d install ; done
+	mkdir -p ${PREFIX}/lib/pkgconfig
+	cp iso-codes.pc ${PREFIX}/lib/pkgconfig/iso-codes.pc
+
+
+iso-codes.pc: iso-codes.pc.in
+	sed     -e 's/@VERSION@/${VERSION}/' \
+		-e 's/@DOMAINS@/${DOMAINS}/' \
+		-e 's/@PREFIX@/${PREFIX_QUOTED}/' <  $< > $@
+
---
+++ iso_3166/iso_3166.h
@@ -0,0 +1,480 @@
+/* AD */ 
+ N_("Andorra")
+/* AE */ 
+ N_("United Arab Emirates")
+/* AF */ 
+ N_("Afghanistan")
+/* AG */ 
+ N_("Antigua and Barbuda")
+/* AI */ 
+ N_("Anguilla")
+/* AL */ 
+ N_("Albania")
+/* AM */ 
+ N_("Armenia")
+/* AN */ 
+ N_("Netherlands Antilles")
+/* AO */ 
+ N_("Angola")
+/* AQ */ 
+ N_("Antarctica")
+/* AR */ 
+ N_("Argentina")
+/* AS */ 
+ N_("American Samoa")
+/* AT */ 
+ N_("Austria")
+/* AU */ 
+ N_("Australia")
+/* AW */ 
+ N_("Aruba")
+/* AZ */ 
+ N_("Azerbaijan")
+/* BA */ 
+ N_("Bosnia and Herzegovina")
+/* BB */ 
+ N_("Barbados")
+/* BD */ 
+ N_("Bangladesh")
+/* BE */ 
+ N_("Belgium")
+/* BF */ 
+ N_("Burkina Faso")
+/* BG */ 
+ N_("Bulgaria")
+/* BH */ 
+ N_("Bahrain")
+/* BI */ 
+ N_("Burundi")
+/* BJ */ 
+ N_("Benin")
+/* BM */ 
+ N_("Bermuda")
+/* BN */ 
+ N_("Brunei Darussalam")
+/* BO */ 
+ N_("Bolivia")
+/* BR */ 
+ N_("Brazil")
+/* BS */ 
+ N_("Bahamas")
+/* BT */ 
+ N_("Bhutan")
+/* BV */ 
+ N_("Bouvet Island")
+/* BW */ 
+ N_("Botswana")
+/* BY */ 
+ N_("Belarus")
+/* BZ */ 
+ N_("Belize")
+/* CA */ 
+ N_("Canada")
+/* CC */ 
+ N_("Cocos (Keeling) Islands")
+/* CD */ 
+ N_("Congo, The Democratic Republic of")
+/* CF */ 
+ N_("Central African Republic")
+/* CG */ 
+ N_("Congo")
+/* CH */ 
+ N_("Switzerland")
+/* CI */ 
+ N_("Cote d'Ivoire")
+/* CK */ 
+ N_("Cook Islands")
+/* CL */ 
+ N_("Chile")
+/* CM */ 
+ N_("Cameroon")
+/* CN */ 
+ N_("China")
+/* CO */ 
+ N_("Colombia")
+/* CR */ 
+ N_("Costa Rica")
+/* CS */ 
+ N_("Serbia and Montenegro")
+/* CU */ 
+ N_("Cuba")
+/* CV */ 
+ N_("Cape Verde")
+/* CX */ 
+ N_("Christmas Island")
+/* CY */ 
+ N_("Cyprus")
+/* CZ */ 
+ N_("Czech Republic")
+/* DE */ 
+ N_("Germany")
+/* DJ */ 
+ N_("Djibouti")
+/* DK */ 
+ N_("Denmark")
+/* DM */ 
+ N_("Dominica")
+/* DO */ 
+ N_("Dominican Republic")
+/* DZ */ 
+ N_("Algeria")
+/* EC */ 
+ N_("Ecuador")
+/* EE */ 
+ N_("Estonia")
+/* EG */ 
+ N_("Egypt")
+/* EH */ 
+ N_("Western Sahara")
+/* ER */ 
+ N_("Eritrea")
+/* ES */ 
+ N_("Spain")
+/* ET */ 
+ N_("Ethiopia")
+/* FI */ 
+ N_("Finland")
+/* FJ */ 
+ N_("Fiji")
+/* FK */ 
+ N_("Falkland Islands (Malvinas)")
+/* FM */ 
+ N_("Micronesia")
+/* FO */ 
+ N_("Faroe Islands")
+/* FR */ 
+ N_("France")
+/* GA */ 
+ N_("Gabon")
+/* GB */ 
+ N_("Britain (UK)")
+/* GD */ 
+ N_("Grenada")
+/* GE */ 
+ N_("Georgia")
+/* GF */ 
+ N_("French Guiana")
+/* GH */ 
+ N_("Ghana")
+/* GI */ 
+ N_("Gibraltar")
+/* GL */ 
+ N_("Greenland")
+/* GM */ 
+ N_("Gambia")
+/* GN */ 
+ N_("Guinea")
+/* GP */ 
+ N_("Guadeloupe")
+/* GQ */ 
+ N_("Equatorial Guinea")
+/* GR */ 
+ N_("Greece")
+/* GS */ 
+ N_("South Georgia and the South Sandwich Islands")
+/* GT */ 
+ N_("Guatemala")
+/* GU */ 
+ N_("Guam")
+/* GW */ 
+ N_("Guinea-Bissau")
+/* GY */ 
+ N_("Guyana")
+/* HK */ 
+ N_("Hong Kong")
+/* HM */ 
+ N_("Heard Island & McDonald Islands")
+/* HN */ 
+ N_("Honduras")
+/* HR */ 
+ N_("Croatia")
+/* HT */ 
+ N_("Haiti")
+/* HU */ 
+ N_("Hungary")
+/* ID */ 
+ N_("Indonesia")
+/* IE */ 
+ N_("Ireland")
+/* IL */ 
+ N_("Israel")
+/* IN */ 
+ N_("India")
+/* IO */ 
+ N_("British Indian Ocean Territory")
+/* IQ */ 
+ N_("Iraq")
+/* IR */ 
+ N_("Iran")
+/* IS */ 
+ N_("Iceland")
+/* IT */ 
+ N_("Italy")
+/* JM */ 
+ N_("Jamaica")
+/* JO */ 
+ N_("Jordan")
+/* JP */ 
+ N_("Japan")
+/* KE */ 
+ N_("Kenya")
+/* KG */ 
+ N_("Kyrgyzstan")
+/* KH */ 
+ N_("Cambodia")
+/* KI */ 
+ N_("Kiribati")
+/* KM */ 
+ N_("Comoros")
+/* KN */ 
+ N_("St Kitts & Nevis")
+/* KP */ 
+ N_("Korea, Democratic People's Republic of")
+/* KR */ 
+ N_("Korea, Republic of")
+/* KW */ 
+ N_("Kuwait")
+/* KY */ 
+ N_("Cayman Islands")
+/* KZ */ 
+ N_("Kazakhstan")
+/* LA */ 
+ N_("Laos")
+/* LB */ 
+ N_("Lebanon")
+/* LC */ 
+ N_("St Lucia")
+/* LI */ 
+ N_("Liechtenstein")
+/* LK */ 
+ N_("Sri Lanka")
+/* LR */ 
+ N_("Liberia")
+/* LS */ 
+ N_("Lesotho")
+/* LT */ 
+ N_("Lithuania")
+/* LU */ 
+ N_("Luxembourg")
+/* LV */ 
+ N_("Latvia")
+/* LY */ 
+ N_("Libya")
+/* MA */ 
+ N_("Morocco")
+/* MC */ 
+ N_("Monaco")
+/* MD */ 
+ N_("Moldova")
+/* MG */ 
+ N_("Madagascar")
+/* MH */ 
+ N_("Marshall Islands")
+/* MK */ 
+ N_("Macedonia")
+/* ML */ 
+ N_("Mali")
+/* MM */ 
+ N_("Myanmar")
+/* MN */ 
+ N_("Mongolia")
+/* MO */ 
+ N_("Macao")
+/* MP */ 
+ N_("Northern Mariana Islands")
+/* MQ */ 
+ N_("Martinique")
+/* MR */ 
+ N_("Mauritania")
+/* MS */ 
+ N_("Montserrat")
+/* MT */ 
+ N_("Malta")
+/* MU */ 
+ N_("Mauritius")
+/* MV */ 
+ N_("Maldives")
+/* MW */ 
+ N_("Malawi")
+/* MX */ 
+ N_("Mexico")
+/* MY */ 
+ N_("Malaysia")
+/* MZ */ 
+ N_("Mozambique")
+/* NA */ 
+ N_("Namibia")
+/* NC */ 
+ N_("New Caledonia")
+/* NE */ 
+ N_("Niger")
+/* NF */ 
+ N_("Norfolk Island")
+/* NG */ 
+ N_("Nigeria")
+/* NI */ 
+ N_("Nicaragua")
+/* NL */ 
+ N_("Netherlands")
+/* NO */ 
+ N_("Norway")
+/* NP */ 
+ N_("Nepal")
+/* NR */ 
+ N_("Nauru")
+/* NU */ 
+ N_("Niue")
+/* NZ */ 
+ N_("New Zealand")
+/* OM */ 
+ N_("Oman")
+/* PA */ 
+ N_("Panama")
+/* PE */ 
+ N_("Peru")
+/* PF */ 
+ N_("French Polynesia")
+/* PG */ 
+ N_("Papua New Guinea")
+/* PH */ 
+ N_("Philippines")
+/* PK */ 
+ N_("Pakistan")
+/* PL */ 
+ N_("Poland")
+/* PM */ 
+ N_("St Pierre and Miquelon")
+/* PN */ 
+ N_("Pitcairn")
+/* PR */ 
+ N_("Puerto Rico")
+/* PS */ 
+ N_("Palestine")
+/* PT */ 
+ N_("Portugal")
+/* PW */ 
+ N_("Palau")
+/* PY */ 
+ N_("Paraguay")
+/* QA */ 
+ N_("Qatar")
+/* RE */ 
+ N_("Reunion")
+/* RO */ 
+ N_("Romania")
+/* RU */ 
+ N_("Russia")
+/* RW */ 
+ N_("Rwanda")
+/* SA */ 
+ N_("Saudi Arabia")
+/* SB */ 
+ N_("Solomon Islands")
+/* SC */ 
+ N_("Seychelles")
+/* SD */ 
+ N_("Sudan")
+/* SE */ 
+ N_("Sweden")
+/* SG */ 
+ N_("Singapore")
+/* SH */ 
+ N_("Saint Helena")
+/* SI */ 
+ N_("Slovenia")
+/* SJ */ 
+ N_("Svalbard and Jan Mayen")
+/* SK */ 
+ N_("Slovakia")
+/* SL */ 
+ N_("Sierra Leone")
+/* SM */ 
+ N_("San Marino")
+/* SN */ 
+ N_("Senegal")
+/* SO */ 
+ N_("Somalia")
+/* SR */ 
+ N_("Suriname")
+/* ST */ 
+ N_("Sao Tome and Principe")
+/* SV */ 
+ N_("El Salvador")
+/* SY */ 
+ N_("Syrian Arab Republic")
+/* SZ */ 
+ N_("Swaziland")
+/* TC */ 
+ N_("Turks & Caicos Is")
+/* TD */ 
+ N_("Chad")
+/* TF */ 
+ N_("French Southern Territories")
+/* TG */ 
+ N_("Togo")
+/* TH */ 
+ N_("Thailand")
+/* TJ */ 
+ N_("Tajikistan")
+/* TK */ 
+ N_("Tokelau")
+/* TL */ 
+ N_("Timor-Leste")
+/* TM */ 
+ N_("Turkmenistan")
+/* TN */ 
+ N_("Tunisia")
+/* TO */ 
+ N_("Tonga")
+/* TR */ 
+ N_("Turkey")
+/* TT */ 
+ N_("Trinidad and Tobago")
+/* TV */ 
+ N_("Tuvalu")
+/* TW */ 
+ N_("Taiwan")
+/* TZ */ 
+ N_("Tanzania")
+/* UA */ 
+ N_("Ukraine")
+/* UG */ 
+ N_("Uganda")
+/* UM */ 
+ N_("United States Minor Outlying Islands")
+/* US */ 
+ N_("United States")
+/* UY */ 
+ N_("Uruguay")
+/* UZ */ 
+ N_("Uzbekistan")
+/* VA */ 
+ N_("Vatican City")
+/* VC */ 
+ N_("St Vincent and the Grenadines")
+/* VE */ 
+ N_("Venezuela")
+/* VG */ 
+ N_("Virgin Islands, British")
+/* VI */ 
+ N_("Virgin Islands, U.S.")
+/* VN */ 
+ N_("Vietnam")
+/* VU */ 
+ N_("Vanuatu")
+/* WF */ 
+ N_("Wallis and Futuna")
+/* WS */ 
+ N_("Samoa")
+/* YE */ 
+ N_("Yemen")
+/* YT */ 
+ N_("Mayotte")
+/* YU */ 
+ N_("Yugoslavia")
+/* ZA */ 
+ N_("South Africa")
+/* ZM */ 
+ N_("Zambia")
+/* ZW */ 
+ N_("Zimbabwe")
