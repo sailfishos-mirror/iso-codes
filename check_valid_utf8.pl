94f4a735be284680dc2d87eecb8fe26e2e6cdb9d
Tobias Quathamer <toddy@debian.org>
2009-04-06 17:46:32 +0200

    Change license to LGPL

--- check_valid_utf8.pl
+++ check_valid_utf8.pl
@@ -3,8 +3,21 @@
 # Takes a list of files on the command line and checks for valid
 # UTF-8 data. Used for checking .po files.
 #
-# Copyright (C) 2009 Tobias Quathamer <toddy@debian.org>
-# Released under the GPL version 2 or later.
+# Copyright © 2009 Tobias Quathamer <toddy@debian.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 use strict;
 use warnings;
--- iso2pot.py
+++ iso2pot.py
@@ -1,10 +1,23 @@
 #!/usr/bin/env python
 #
 # Read iso-codes data file and output a .pot file
-# 
-# Copyright (C) 2004,2005 Alastair McKinstry <mckinstry@debian.org>
-# Copyright (C) 2006,2007 Tobias Quathamer <toddy@debian.org>
-# Released under the GPL.
+#
+# Copyright © 2004,2005 Alastair McKinstry <mckinstry@debian.org>
+# Copyright © 2006,2007 Tobias Quathamer <toddy@debian.org>
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU Lesser General Public
+# License as published by the Free Software Foundation; either
+# version 2.1 of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 from xml.sax import make_parser, SAXException, SAXParseException
 from xml.sax.handler import feature_namespaces, ContentHandler
