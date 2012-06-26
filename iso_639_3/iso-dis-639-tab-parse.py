8aaf9ef92fe3a71ac42b894f741cb85f51bbf8de
Tobias Quathamer <toddy@debian.org>
2012-06-26 09:35:39 +0200

    Use the new file format from sil.org for inverted names

--- iso_639_3/iso-dis-639-tab-parse.py
+++ iso_639_3/iso-dis-639-tab-parse.py
@@ -23,13 +23,49 @@ Parse the SIL.org iso_639_3.tab file and create
 an XML file for our own use.
 """
 
-inverted_names = {}
-names=open("iso_639_3_Name_Index.tab")
+# The Name_Index file has the same fields as the normal
+# tab file. Extract the additional entries and save them
+# for later.
+alternative_names = {}
+last_code = ''
+last_name = ''
+names = open("iso_639_3_Name_Index.tab")
 for li in names.readlines():
-	(code, name, inverted_name) = li.split('\t')
-	inverted_name = inverted_name.strip()
-	if inverted_name != name:
-		inverted_names[code] = inverted_name
+	# Split the line into parts
+	parts = li.split('\t')
+	# Reverse the parts, because Python's pop() function is much
+	# faster at the end of a list instead of at the start of a list
+	parts.reverse()
+	# Get the code
+	code = parts.pop()
+	# Take away unneeded parts
+	status = parts.pop()
+	partner_agency = parts.pop()
+	iso_639_3 = parts.pop()
+	iso_639_2 = parts.pop()
+	b_code = parts.pop()
+	bt_equiv = parts.pop()
+	iso_639_1 = parts.pop()
+	# At this point, we are at 'reference_name'. This field may
+	# contain a quote sign, so we have to look for it and append
+	# the next field, completing the field.
+	reference_name = parts.pop()
+	if reference_name.startswith('"'):
+		reference_name = reference_name + parts.pop()
+		# Now strip the quote signs
+		reference_name = reference_name.strip('"')
+	# If we have already seen the code, we store the name
+	# for later use, else skip the rest
+	if code == last_code:
+		if code in alternative_names:
+			# If there is already an entry, just append the current name
+			alternative_names[code].append(reference_name)
+		else:
+			# The is no entry yet, so save the previous name, too
+			alternative_names[code] = [last_name, reference_name]
+	# Store code and reference_name for comparison
+	last_code = code
+	last_name = reference_name
 names.close()
 
 # Set up a dictionary for the one letter abbreviations
@@ -45,7 +81,11 @@ def create_iso_639_3_entry(entry):
 		result += '\t\tpart2_code="%s"\n' % entry['iso_639_2']
 	result += '\t\tscope="%s"\n' % entry['element_scope']
 	result += '\t\ttype="%s"\n' % entry['language_type']
-	result += '\t\tname="%s" />\n' % entry['reference_name']
+	# Use the inverted form for the name attribute
+	if 'inverted_name' in entry:
+		result += '\t\tname="%s" />\n' % entry['inverted_name']
+	else:
+		result += '\t\tname="%s" />\n' % entry['reference_name']
 	return result
 
 tabular_file = open("iso_639_3.tab")
@@ -101,7 +141,7 @@ tabular_file.readline()
 iso_639_3_entry = {}
 
 for li in tabular_file.readlines():
-	# Split the line into parts and look for quotes
+	# Split the line into parts
 	parts = li.split('\t')
 	# Reverse the parts, because Python's pop() function is much
 	# faster at the end of a list instead of at the start of a list
@@ -138,9 +178,17 @@ for li in tabular_file.readlines():
 	iso_639_3_entry['iso_639_2'] = iso_639_2
 	iso_639_3_entry['element_scope'] = element_scope
 	iso_639_3_entry['language_type'] = language_type
-	if inverted_names.has_key(code):
-		reference_name = inverted_names[code]
 	iso_639_3_entry['reference_name'] = reference_name
+	# If there are alternative names, try to find the one
+	# with a comma, which is treated as the inverted name.
+	if code in alternative_names:
+		for other_name in alternative_names[code]:
+			# Skip the reference_name
+			if other_name == reference_name:
+				continue
+			# If there is a comma, use this as the inverted form
+			if ',' in other_name:
+				iso_639_3_entry['inverted_name'] = other_name
 
 # Finally, write the last entry and close the XML file
 entry = create_iso_639_3_entry(iso_639_3_entry)
