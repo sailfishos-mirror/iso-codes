559b2a12026ddb9bc60602f83e8d4f9e3093e88f
Dr. Tobias Quathamer <toddy@debian.org>
2025-11-18 14:42:57 +0100

    Include SPDX annotation

--- scripts/iso-dis-639-tab-parse.py
+++ scripts/iso-dis-639-tab-parse.py
@@ -1,21 +1,9 @@
 #!/usr/bin/python3
+
+# SPDX-FileCopyrightText: 2005 Alastair McKinstry <mckinstry@computer.org>
+# SPDX-FileCopyrightText: 2008,2012,2013,2022 Dr. Tobias Quathamer <toddy@debian.org>
 #
-# Copyright © 2005 Alastair McKinstry <mckinstry@computer.org>
-# Copyright © 2008,2012,2013,2022 Dr. Tobias Quathamer <toddy@debian.org>
-#
-# This file is free software; you can redistribute it and/or
-# modify it under the terms of the GNU Lesser General Public
-# License as published by the Free Software Foundation; either
-# version 2.1 of the License, or (at your option) any later version.
-#
-# This file is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-# Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program.
-# If not, see https://www.gnu.org/licenses/.
+# SPDX-License-Identifier: LGPL-2.1-or-later
 
 """
 Parse the SIL.org iso_639_3.tab file and create
